7ed6a379a717aff0b18f074c31401b0cc935b40d
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-08-31 14:03:11 +0200

    Set up gpu

--- utils/set_up_gpu.py
+++ utils/set_up_gpu.py
@@ -1,9 +1,10 @@
 import plaidml.keras
+import os
 
 plaidml.keras.install_backend()
 
 # Help MacOS be able to use Keras
-import os
 os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 # Gets rid of the processor warning.
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
