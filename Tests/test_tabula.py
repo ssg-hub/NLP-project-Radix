17b4f405291ddf842bf56cb766c23e1ab2622f70
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-09-03 15:01:47 +0200

    Delete test_tabula.py

--- Tests/test_tabula.py
+++
@@ -1,7 +0,0 @@
-# Import the required Module
-import tabula
-# Read a PDF File
-df = tabula.read_pdf("/Users/paww/Documents/GitHub/NLP-project-Radix/assets/pdf/14.pdf", pages='all')[0]
-# convert PDF into CSV
-tabula.convert_into("/Users/paww/Documents/GitHub/NLP-project-Radix/assets/pdf/14.pdf", "/Users/paww/Documents/GitHub/NLP-project-Radix/assets/pdf/14.csv", output_format="csv", pages='all')
-print(df)
