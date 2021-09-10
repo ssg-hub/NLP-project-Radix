cf2c6d2cc678896a7b04661a711c14deb9b00d8e
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-09-10 09:50:52 +0200

    Cleaning

--- Tests/extract_pages_test.py
+++
@@ -1,4 +0,0 @@
-from pdfminer.high_level import extract_pages
-for page_layout in extract_pages("pdf/27.pdf"):
-    for element in page_layout:
-        print(element)
--- Tests/temp.py
+++
@@ -1,4 +0,0 @@
-import spacy
-nlp = spacy.load("en_core_web_sm")
-doc = nlp("This is a sentence.")
-print(doc)
